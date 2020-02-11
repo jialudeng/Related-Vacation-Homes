3ccc7ec51b5068790fea3e804ddb8321f074c387
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-02-10 18:04:55 -0800

    update webpack to production mode

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('recommendations'));
+ReactDOM.render(<App/>, document.getElementById("recommendations"));
+
+/*
+bundle
+- code for app.
+- get requests relative routes /gallery/32
+- proxy needs to serve  the route, and goto http://13.57.225.193:3000/gallery/32
+*/
